1172ae706423e401476eca75e841364dec36abac
João Mangueira <jamjs7@gmail.com>
2021-04-03 10:58:31 -0300

    add rotas de specifications

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post('/', (request, response) => {
+    const { name, description } = request.body;
+
+    const createCategoryService = new CreateSpecificationService(specificationsRepository);
+
+    createCategoryService.execute({ name, description });
+    return response.status(201).send();
+
+});
+
+export { specificationsRoutes };
