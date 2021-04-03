6587614404537da2a9a0cd5b7a7c0511530cc040
João Mangueira <jamjs7@gmail.com>
2021-04-03 11:30:56 -0300

    usando Singleton como estrategia para os repositorios

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 import { ListCategoriesController } from './ListCategoriesController';
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
