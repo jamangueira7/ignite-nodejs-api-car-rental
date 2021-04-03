14989cfce70e8483755a5d430f8f0f30db96d02a
João Mangueira <jamjs7@gmail.com>
2021-04-02 21:43:47 -0300

    rota de criação de categorias pronta

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,19 @@
-import {response, Router} from 'express';
+import { Router } from 'express';
 
 const categoriesRoutes = Router();
 
+const categories = [];
+
 categoriesRoutes.post('/categories', (request, response) => {
     const { name, description } = request.body;
+
+    categories.push({
+        name,
+        description
+    });
+
+    return response.status(201).send();
+
 });
 
 export { categoriesRoutes };
