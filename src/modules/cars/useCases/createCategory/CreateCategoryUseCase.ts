d13c475e512b9839704c1df48f83b9be7375e6ec
João Mangueira <jamjs7@gmail.com>
2021-04-18 22:12:32 -0300

    projeto não está rodando

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -17,6 +17,7 @@ class CreateCategoryUseCase {
     ) {}
 
     async execute({ name, description }: IRequest): Promise<void> {
+
         const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
 
         if(categoryAlreadyExists) {
