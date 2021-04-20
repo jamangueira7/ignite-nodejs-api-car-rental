a9ee43388ac47e6b12751949051c944d64e4061c
João Mangueira <jamjs7@gmail.com>
2021-04-19 22:31:53 -0300

    Criando seed de admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,12 +5,12 @@ import swaggerUi from 'swagger-ui-express';
 
 
 
+import '@shared/container';
 import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
+
 import swaggerFile from '../../../swagger.json';
 import { router } from '@shared/infra/http/routes';
-
-import '@shared/infra/typeorm';
-import '@shared/container';
 
 
 const app = express();
--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -2,15 +2,17 @@ import { hash } from 'bcryptjs';
 import { getConnection } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
+import createConnection from '@shared/infra/typeorm';
+
 async function create() {
-    const connection = getConnection();
+    const connection = await createConnection('localhost');
 
     const id = uuidv4();
     const password = await hash("admin", 8);
 
     await connection.query(
-        `INSERT INTO USERS(id, name, email, password, admin, created_at)
-                VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, ${new Date()})
+        `INSERT INTO USERS(id, name, email, password, "isAdmin", driver_license, created_at)
+                VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'XXXXX', 'now()')
             `
     )
 }
