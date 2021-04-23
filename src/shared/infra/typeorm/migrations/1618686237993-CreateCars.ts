19bd8a0548b376e347ede53282b96839874f3fa6
João Mangueira <jamjs7@gmail.com>
2021-04-23 20:38:41 -0300

    criando migration speficication_car

--- src/shared/infra/typeorm/migrations/1618686237993-CreateCars.ts
+++ src/shared/infra/typeorm/migrations/1618686237993-CreateCars.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table, TableColumn} from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateCars1618686237993 implements MigrationInterface {
 
