bcac2ed695617260aa99bf085f1cb6fd1547296b
João Mangueira <jamjs7@gmail.com>
2021-05-08 10:46:53 -0300

    criando toda a estrutura no modulo rental para criar o primeiro teste

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,14 +1,21 @@
 import { CreateRentalUseCase } from '@modules/rentals/useCases/createRental/CreateRentalUseCase';
+import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
 
 let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 
 describe('Create Rental', () => {
 
     beforeEach(() => {
-        createRentalUseCase = new CreateRentalUseCase();
+        rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
     });
 
     it('should be able to create a new rental', async () => {
-        await createRentalUseCase.execute();
+        await createRentalUseCase.execute({
+            user_id: "12345",
+            car_id: "121212",
+            expected_return_date: new Date(),
+        });
     });
 })
