96861dbc8bebf8881b137341a8e8a9239fa20e32
João Mangueira <jamjs7@gmail.com>
2021-05-08 13:19:48 -0300

    Alterando migration createRentals e implementando o typeorm no Rental

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,6 +1,7 @@
 import {
     Column,
     CreateDateColumn,
+    UpdateDateColumn,
     Entity,
     JoinColumn,
     ManyToOne,
@@ -10,23 +11,33 @@ import {
 
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity("rentals")
 class Rental {
+    @PrimaryColumn()
     id?: string;
 
+    @Column()
     car_id: string;
 
+    @Column()
     user_id: string;
 
+    @Column()
     start_date: Date;
 
+    @Column()
     end_date: Date;
 
+    @Column()
     expected_return_date: Date;
 
+    @Column()
     total: number;
 
+    @CreateDateColumn()
     created_at: Date;
 
+    @UpdateDateColumn()
     updated_at: Date;
 
     constructor() {
