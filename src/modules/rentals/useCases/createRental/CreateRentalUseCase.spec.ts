513e850c3e7ad95fb55db8bdc2c97ca8743836f5
João Mangueira <jamjs7@gmail.com>
2021-05-08 10:29:55 -0300

    criando createRental

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,14 @@
+import { CreateRentalUseCase } from '@modules/rentals/useCases/createRental/CreateRentalUseCase';
+
+let createRentalUseCase: CreateRentalUseCase;
+
+describe('Create Rental', () => {
+
+    beforeEach(() => {
+        createRentalUseCase = new CreateRentalUseCase();
+    });
+
+    it('should be able to create a new rental', async () => {
+        await createRentalUseCase.execute();
+    });
+})
---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,13 @@
+
+
+interface IRequest {
+    user_id: string;
+    car_id: string;
+    expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+    async execute(): Promise<void>;
+}
+
+export { CreateRentalUseCase };
