a9f75bccf07682d679b0013bfb798efd8285e396
João Mangueira <jamjs7@gmail.com>
2021-06-04 19:02:18 -0300

    devolução de carro

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -28,12 +28,46 @@ class DevolutionRentalUseCase {
     async execute({ id, car_id }: IRequest): Promise<Rental>{
 
         const rental = await this.rentalsRepository.findById(id);
+        const car = await this.carsRepository.findById(car_id);
+        const minimum_daily = 1;
 
         if(!rental) {
             throw new AppError("Rental does not exists!");
         }
 
         //Verificar o tempo do aluguel
+        const dateNow = this.dateProvider.dateNow();
+
+        let daily = this.dateProvider.compareInDays(
+            rental.start_date,
+            dateNow
+        );
+
+        if(daily <= 0 ) {
+            daily = minimum_daily;
+        }
+
+        const delay = this.dateProvider.compareInDays(
+            dateNow,
+            rental.expected_return_date
+        );
+
+        let total = 0;
+
+        if(delay > 0) {
+            total = daily * car.fine_amount;
+        }
+
+        total += daily * car.daily_rate;
+
+        rental.end_date = dateNow;
+        rental.total = total;
+
+        await this.rentalsRepository.create(rental);
+        await this.carsRepository.updateAvailable(car.id, true);
+
+        return rental;
+
     }
 }
 
